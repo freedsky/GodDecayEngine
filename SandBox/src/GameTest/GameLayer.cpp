5f7e01cbaa2f3ff81231092047d94df4bbfda59c
doumping <doumping@engine.com>
2023-10-09 19:00:15 +0800

    new Performance Analyser , promote redner2d

--- SandBox/src/GameTest/GameLayer.cpp
+++ SandBox/src/GameTest/GameLayer.cpp
@@ -24,18 +24,33 @@ void GameLayer::OnDetach()
 
 void GameLayer::OnUpDate(float deltaTime)
 {
-	m_Camera->OnUpdate(deltaTime);
+	GD_PROFILE_FUNCTION();
+	{
+		GD_PROFILE_SCOPE("CameraController::OnUpdate");
+		m_Camera->OnUpdate(deltaTime);
+	}
+	
+	{
+		GD_PROFILE_SCOPE("Renderer Prep");
+		GodDecay::RenderCommand::SetClearColor(glm::vec4(0.1, 0.1, 0.1, 1.0));
+		GodDecay::RenderCommand::Clear();
+	}
+	
+	{
+		GD_PROFILE_SCOPE("Renderer Draw");
+		GodDecay::Renderer2D::BeginScene(*m_Camera->GetCamera().get());
 
-	GodDecay::RenderCommand::SetClearColor(glm::vec4(0.1, 0.1, 0.1, 1.0));
-	GodDecay::RenderCommand::Clear();
+		GodDecay::Renderer2D::DrawQuad(glm::vec2(0.0f, 0.0f), glm::vec2(1.0f, 1.0f), m_TextureColor);
 
-	GodDecay::Renderer2D::BeginScene(*m_Camera->GetCamera().get());
+		GodDecay::Renderer2D::DrawRotatedQuad(glm::vec2(-2.0f, -2.0f), glm::vec2(1.0f, 1.0f), 45.0f,m_TextureColor);
 
-	GodDecay::Renderer2D::DrawQuad(glm::vec2(0.0f, 0.0f), glm::vec2(1.0f,1.0f), m_TextureColor);
+		GodDecay::Renderer2D::DrawQuad(glm::vec2(1.0f, 1.0f), glm::vec2(1.0f, 1.0f), m_SquareTexture, m_TextureColor);
 
-	GodDecay::Renderer2D::DrawQuad(glm::vec2(1.0f, 1.0f), glm::vec2(1.0f, 1.0f), m_SquareTexture);
+		GodDecay::Renderer2D::DrawRotatedQuad(glm::vec2(2.0f, 2.0f), glm::vec2(1.0f, 1.0f),45.0f ,m_SquareTexture, m_TextureColor);
 
-	GodDecay::Renderer2D::EndScene();
+		GodDecay::Renderer2D::EndScene();
+	}
+	
 }
 
 void GameLayer::OnEvents(GodDecay::Event& e)
